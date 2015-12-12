b786c320bf90540739c80fea2f66f2c25012738c
AbdielMelendez/ <abdielhmelendez@gmail.com>
2015-12-12 12:19:35 -0800

    Added first quote

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.getElementById('Button').onclick = function() {
-   alert("button was clicked");
-}
-/* comment's on my script*/
-//New comments
+var quotes = ["At least two thirds of our miseries spring from
+human stupidity, human maliceand those great motivators and
+justifiers of malice and stupidity: idealism, dogmatism and
+proselytizing zeal on behalf of religious or political ideas
+"
+]
