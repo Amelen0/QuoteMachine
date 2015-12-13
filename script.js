c3d840da086b935ec39913bfaaa59182d45add14
AbdielMelendez/ <abdielhmelendez@gmail.com>
2015-12-12 16:03:23 -0800

    Finally got the button to hide

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var quotes = ["At least two thirds of our miseries spring from
-human stupidity, human maliceand those great motivators and
-justifiers of malice and stupidity: idealism, dogmatism and
-proselytizing zeal on behalf of religious or political ideas
-"
-]
+$(document).ready(function (){
+  $("button").click(function(){
+    $(this).hide();
+  })
+})
